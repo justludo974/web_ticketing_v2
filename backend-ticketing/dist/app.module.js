61ada1e6a96ec0ebca7b0fcc9b6e45629096eb34
justludo974 <knishawdomen@gmail.com>
2021-08-20 16:18:52 +0400

    backend_db_connection

--- backend-ticketing/dist/app.module.js
+++ backend-ticketing/dist/app.module.js
@@ -10,11 +10,14 @@ exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
+const mongoose_1 = require("@nestjs/mongoose");
 let AppModule = class AppModule {
 };
 AppModule = __decorate([
     common_1.Module({
-        imports: [],
+        imports: [
+            mongoose_1.MongooseModule.forRoot('mongodb://localhost/backend-ticketing', { useNewUrlParser: true }),
+        ],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
